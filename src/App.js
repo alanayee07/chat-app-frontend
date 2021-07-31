13aac2b172be9ff0a6ee8a66110908d60f287a7c
Alana <alanayee07@gmail.com>
2021-07-31 12:00:35 -0700

    added send Message on socket

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, {useState} from 'react';
+import io from 'socket.io-client';
+
+
+const socket = io('http://localhost:4001');
 
 const App = () => {
 
   const [message, setMessage] = useState('')
+
   const sendMessage = e => {
     e.preventDefault();
+    console.log(message);
+    socket.emit('message', {message});
+    // send message on socket
+    setMessage('');
   }
 
   return (
@@ -18,6 +27,7 @@ const App = () => {
           value={message}
           onChange={e => setMessage(e.target.value)}
         />
+        <button type="submit">Send</button>
       </form>
     </div>
   )
