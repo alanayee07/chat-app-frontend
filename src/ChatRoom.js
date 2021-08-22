9ec296e9e5bd51de417ccf098005cbc31ef9f906
Alana <alanayee07@gmail.com>
2021-08-22 10:32:19 -0700

    Created useEffect to emit message obj

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -1,10 +1,22 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import SocketIo from './socket.io.client/index.js';
 
 const ChatRoom = (props) => {
   const { roomId } = props.match.params;
 
+  const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    SocketIo.on('message', msg => {
+      setMessages([...messages, msg]);
+    })
+  }, [messages]);
+
+  useEffect(() => {
+    SocketIo.emit('message', {userId: SocketIo.id, room: roomId})
+  }, [roomId]);
+
 
   const sendMessage = e => {
     e.preventDefault();
@@ -20,6 +32,15 @@ const ChatRoom = (props) => {
   return (
     <div className="chatRoom-container">
       <h1>Room: {roomId}</h1>
+      <div className="message-container">
+        <div className="message-list">
+          {messages.map((msg, index)=> {
+            return (
+              <h4 key={index}>{msg.message}</h4>
+            )
+          })}
+        </div>
+      </div>
       <form onSubmit={sendMessage}>
         <input
           type="text"
