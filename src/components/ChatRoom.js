6d84791bb6673fd7a239fe6ec18287a3f1f51370
Alana <alanayee07@gmail.com>
2021-09-07 17:35:08 -0700

    added chatRoom-inner div, updated display flexes

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -19,7 +19,7 @@ const ChatRoom = ({location}) => {
 
   useEffect(() => {
     SocketIo.on('join', userMap => {
-      setOnlineUsers({...onlineUsers, userMap});
+      setOnlineUsers(userMap);
     })
   }, [onlineUsers]);
 
@@ -62,44 +62,46 @@ const ChatRoom = ({location}) => {
 
   return (
     <div className="chatRoom-container">
-      <OnlineUsers onlineUsers={onlineUsers}/>
-      <div className="message-container">
-        <h5 className="room-header">Room: {roomName}</h5>
-        <div className="message-list">
-          {messages.map((msg, index) => {
-            let chatMsgClassName = '';
-            if (!msg.message) {
-              return null;
-            }
-            if (msg.id === SocketIo.id) {
-              chatMsgClassName = '-self';
-            }
-            return (
-              <div className={`msg-container${chatMsgClassName}`} key={`${msg.timestamp}${index}`}>
-              <div className={`msg-timestamp${chatMsgClassName}`}>{moment(msg.timestamp).format("hh:mm")}</div>
-              {(msg.id !== SocketIo.id) && !msg.isChatBot &&  <div className={`msg-username${chatMsgClassName}`}>{msg.username}{':'}</div>}
-              <div className={`msg-message${chatMsgClassName}`}>{msg.message}</div>
-            </div>
-            )
-          })}
-        </div>
-      </div>
-      <div className="sendMessage-container">
-        <div className="sendMessage-formContainer">
-          <form onSubmit={sendMessage}>
-            <input
-              type="text"
-              placeholder="Type Message Here..."
-              value={message}
-              onChange={e => setMessage(e.target.value)}
-              required
-            />
-            <button type="submit">Send</button>
-          </form>
-        </div>
-      </div>
+      <h5 className="room-header">Room: {roomName}</h5>
       <div className="new-room">
         <JoinRoom username={username} />
+      </div>
+      <div className="chatRoom-inner">
+        <div className="message-container">
+          <div className="message-list">
+            {messages.map((msg, index) => {
+              let chatMsgClassName = '';
+              if (!msg.message) {
+                return null;
+              }
+              if (msg.id === SocketIo.id) {
+                chatMsgClassName = '-self';
+              }
+              return (
+                <div className={`msg-container${chatMsgClassName}`} key={`${msg.timestamp}${index}`}>
+                <div className={`msg-timestamp${chatMsgClassName}`}>{moment(msg.timestamp).format("hh:mm")}</div>
+                {(msg.id !== SocketIo.id) && !msg.isChatBot &&  <div className={`msg-username${chatMsgClassName}`}>{msg.username}{':'}</div>}
+                <div className={`msg-message${chatMsgClassName}`}>{msg.message}</div>
+              </div>
+              )
+            })}
+          </div>
+          <div className="sendMessage-container">
+            <div className="sendMessage-formContainer">
+              <form onSubmit={sendMessage}>
+                <input
+                  type="text"
+                  placeholder="Type Message Here..."
+                  value={message}
+                  onChange={e => setMessage(e.target.value)}
+                  required
+                />
+                <button type="submit">Send</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <OnlineUsers onlineUsers={onlineUsers}/>
       </div>
     </div>
   )
--- src/components/OnlineUsers.js
+++ src/components/OnlineUsers.js
@@ -1,11 +1,30 @@
 import React from 'react';
-// import SocketIo from '../socket.io.client/index.js';
+
+import './css/OnlineUsers.css'
 
 const OnlineUsers = (onlineUsers) => {
 
+  const usernames = [];
+
+  const getEachOnlineUser = (onlineUsers) => {
+    console.log('this is the onlineObj', onlineUsers.onlineUsers)
+
+    for (let user in onlineUsers.onlineUsers) {
+      usernames.push(onlineUsers.onlineUsers[user]);
+    }
+    return usernames;
+  }
+  getEachOnlineUser(onlineUsers)
+
   return (
     <div className="onlineUsers-container">
-      {console.log(onlineUsers)}
+      <div className="onlineUsers-list">
+        {usernames.map((user, index) => {
+          return (
+            <div key={`${user}${index}`}>{user}</div>
+          )
+        })}
+      </div>
     </div>
   )
 }
