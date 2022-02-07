ed485d1108de427c2605cd520a2f2e986ce9de25
Alana <alanayee07@gmail.com>
2022-02-07 14:25:36 -0800

    ready for heroku

--- src/socket.io.client/index.js
+++ src/socket.io.client/index.js
@@ -1,5 +1,5 @@
 import SocketIoClient from 'socket.io-client';
-const server = process.env.NODE_ENV === 'development' ? 'http://localhost:7000' : '/';
+const server = process.env.NODE_ENV === 'development' ? 'http://localhost:7000' : 'https://ary-chat-app.herokuapp.com/';
 const SocketIo = SocketIoClient(server);
 
 console.log('this is server: ', process.env.NODE_ENV)
